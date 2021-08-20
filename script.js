aa662302947585ddc21d1879027875936c37e8f4
Christian Foster <fosterchris000@gmail.com>
2021-08-20 14:31:17 -0400

    Refactored javascript

--- script.js
+++ script.js
@@ -5,24 +5,20 @@ const leftArrow = document.querySelector(
   "body > div > div.main-window > div.game-row > div.game-row-arrows > a:nth-child(1) > i"
 );
 
+const cardContainer = document.querySelector(".card-container");
+const cardWrapper = document.querySelector(".card-wrapper");
+const cardOne = document.querySelector(".row-card");
+const cardSize = cardOne.offsetWidth + 4 + 20;
+
 let start = 0;
 
 rightArrow.addEventListener("click", (e) => {
-  const gameRow = e.target.parentElement.parentElement.parentElement;
-  const cardContainer = gameRow.querySelector(".card-container");
-  console.log(cardContainer);
-  let cardSize = document.querySelector(".row-card").offsetWidth + 4 + 20;
   start -= cardSize * 4;
   showCardCode();
   cardContainer.style.left = `${start}px`;
 });
 
 leftArrow.addEventListener("click", (e) => {
-  const gameRow = e.target.parentElement.parentElement.parentElement;
-  const cardContainer = gameRow.querySelector(".card-container");
-  console.log(cardContainer);
-  /* Default is 264 , move forward 4 if 1056*/
-  let cardSize = document.querySelector(".row-card").offsetWidth + 4 + 20;
   start += cardSize * 4;
   cardContainer.style.left = `${start}px`;
   setTimeout(() => {
@@ -30,27 +26,15 @@ leftArrow.addEventListener("click", (e) => {
   }, 2000);
 });
 
-const testCardWrapper = document.querySelector(
-  "body > div > div.main-window > div.game-row > div.card-wrapper"
-);
-const imgBreak = document.querySelector(
-  "body > div > div.main-window > div.game-row > div.card-wrapper > div > div:nth-child(5)"
-);
-
 window.addEventListener("resize", () => {
   showCardCode();
 });
 
 function showCardCode() {
-  let wrapperWidth = testCardWrapper.offsetWidth;
-  let cards = testCardWrapper.querySelectorAll(".card-container .row-card");
-  let cardContainer = testCardWrapper.querySelector(".card-container");
+  let wrapperWidth = cardWrapper.offsetWidth;
+  let cards = cardWrapper.querySelectorAll(".card-container .row-card");
   cards.forEach((card, idx) => {
     let cardEnd = card.offsetWidth + card.offsetLeft;
-    if (idx == 4) {
-      console.log("cardEnd = ", cardEnd + start);
-      console.log("wrapperWidth =", wrapperWidth);
-    }
     if (cardEnd + start + 2 > wrapperWidth) {
       card.style.visibility = "hidden";
       // card.style.display = "none !important";
