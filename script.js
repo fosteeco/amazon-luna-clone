0b0810ff3771a2f15bc2ead71478042ea52d0118
Christian Foster <fosterchris000@gmail.com>
2021-08-25 13:56:42 -0400

    implemented the sliders with grid instead

--- script.js
+++ script.js
@@ -4,47 +4,61 @@ const rightArrow = document.querySelector(
 const leftArrow = document.querySelector(
   "body > div > div.main-window > div.game-row > div.game-row-arrows > a:nth-child(1) > i"
 );
+let rowCount = 1;
 
 const cardContainer = document.querySelector(".card-container");
 const cardWrapper = document.querySelector(".card-wrapper");
 const cardOne = document.querySelector(".row-card");
 const cardSize = cardOne.offsetWidth + 4 + 20;
+cardWrapper.style.height = `${cardOne.clientHeight}px`;
+let cardContainerWidth = cardContainer.clientWidth + 4;
+const gridWrapper = document.querySelector(".grid-wrapper");
+gridWrapper.style.transition = "left 1s ease-in-out";
 
-let start = 0;
+const testBtn = document.querySelector(".test-btn");
 
+function calcContainerWidth() {
+  cardContainerWidth = cardContainer.clientWidth + 4;
+}
+
+let scaleBy;
 rightArrow.addEventListener("click", (e) => {
-  start -= cardSize * 4;
-  showCardCode();
-  cardContainer.style.left = `${start}px`;
+  calcContainerWidth();
+  scaleBy = cardContainerWidth * rowCount;
+  gridWrapper.style.left = `${scaleBy * -1}px`;
+  rowCount++;
+  console.log(rowCount);
 });
 
 leftArrow.addEventListener("click", (e) => {
-  start += cardSize * 4;
-  cardContainer.style.left = `${start}px`;
-  setTimeout(() => {
-    showCardCode();
-  }, 2000);
+  calcContainerWidth();
+  rowCount--;
+  console.log(rowCount);
+  scaleBy = cardContainerWidth * rowCount;
+  scaleBy -= cardContainerWidth;
+  gridWrapper.style.left = `${scaleBy * -1}px`;
 });
 
 window.addEventListener("resize", () => {
-  showCardCode();
+  cardWrapper.style.height = `${cardOne.clientHeight}px`;
+  calcLeft();
 });
 
-function showCardCode() {
-  let wrapperWidth = cardWrapper.offsetWidth;
-  let cards = cardWrapper.querySelectorAll(".card-container .row-card");
-  cards.forEach((card, idx) => {
-    let cardEnd = card.offsetWidth + card.offsetLeft;
-    if (cardEnd + start + 2 > wrapperWidth) {
-      card.style.visibility = "hidden";
-      // card.style.display = "none !important";
-      // card.classList.add("d-none");
-    } else {
-      card.style.visibility = "visible";
-      // card.style.display = "inline-block !important";
-      // card.classList.remove("d-none");
-    }
-  });
+function calcLeft() {
+  calcContainerWidth();
+  let left = rowCount * cardContainerWidth;
+  gridWrapper.style.transition = "";
+  left -= cardContainerWidth;
+  gridWrapper.style.left = `${left * -1}px`;
+  setTimeout(() => {
+    gridWrapper.style.transition = "left 1s ease-in-out";
+  }, 1000);
 }
 
-showCardCode();
+let b = 1;
+testBtn.addEventListener("click", () => {
+  let scaleBy = cardContainerWidth * b;
+  scaleBy += b * 4;
+  gridWrapper.style.left = `${scaleBy * -1}px`;
+  b++;
+});
